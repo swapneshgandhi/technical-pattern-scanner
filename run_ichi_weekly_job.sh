454baf6ea516891f6800d5d81020544258758f85
sgandhi <sgandhi@ooyala.com>
2017-01-14 22:28:38 -0800

    add weekly ichimoku reporting for indian stock market

---
+++ run_ichi_weekly_job.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+date_today=`date +%Y-%m-%d`
+cd /Users/sgandhi/repos/rwork/
+if [[  ! `grep "$date_today" doneFile_weekly` ]] ;then
+	  mv india_stock_list_outfile.txt india_stock_list_outfile_prev.txt
+    /usr/local/bin/Rscript weekly_stock_signals.R
+    if [ $? -eq 0 ];then
+      echo -e 'mhane thano sinha huno hai!\n' | /usr/local/bin/gpg --no-tty --passphrase-fd 0 pd.gpg
+      pd=`cat pd` && rm pd
+      # sort outfile.txt -o outfile.txt
+      # diff outfile_prev.txt outfile.txt > diff_yest.txt
+      /Users/sgandhi/repos/mailsend/mailsend -f swapneshgandhi@gmail.com -smtp 173.194.202.108 -port 587 -starttls -auth-login -user swapneshgandhi@gmail.com -pass $pd \-t swapneshgandhi@gmail.com -sub "ichimoku weekly report" -attach "/Users/sgandhi/repos/rwork/india_stock_list_outfile.txt"
+    fi
+    if [ $? -eq 0 ];then
+      echo $date_today >> doneFile_weekly
+    fi
+fi
+
---
+++ weekly_stock_signals.R
@@ -0,0 +1,69 @@
+require(quantmod)
+require(IKTrading)
+require(Quandl)
+
+args<-commandArgs(TRUE)
+
+"ichimoku" <- function(hlcDaily, nFast=9, nMed=26, nSlow=52) {
+  HLC <- to.weekly(hlcDaily)
+  
+  if (nrow(HLC) >= nSlow){
+  
+  turningLine <- (runMax(Hi(HLC), nFast)+runMin(Lo(HLC), nFast))/2
+  baseLine <- (runMax(Hi(HLC), nMed)+runMin(Lo(HLC), nMed))/2
+  spanA <- lag((turningLine+baseLine)/2, nMed)
+  spanB <- lag((runMax(Hi(HLC), nSlow)+runMin(Lo(HLC), nSlow))/2, nMed)
+  # plotSpan <- lag(Cl(HLC), -nMed) #for plotting the original Ichimoku only
+  laggingSpan <- lag(Cl(HLC), nMed)
+  close <- HLC[,4]
+  out <- cbind(close=close, turnLine=turningLine, baseLine=baseLine, spanA=spanA, spanB=spanB)
+  colnames(out) <- c("close","turnLine", "baseLine", "spanA", "spanB")
+  return (out)
+  }
+}
+
+"anyGood" <- function(ic, quote, name) {
+  
+    limit = 10.0
+    close = tail(ic, n=1)[,1]
+    line1 = tail(ic, n=1)[,2]
+    line2 = tail(ic, n=1)[,3]
+    spanA = tail(ic, n=1)[,4]
+    spanB = tail(ic, n=1)[,5]
+    
+    if(!is.null(spanB) && is.numeric(spanB)){
+    
+      lowerBand = if (spanA < spanB) spanA else spanB
+      higherBand = if (spanB >= spanA) spanB else spanA
+    
+      if (abs(close-higherBand)*100/close <= limit) {
+        cat(paste(quote,name,"close within" , limit, "percent of higherBand", sep=" "))
+        cat("\n")
+      }
+    }
+}
+
+start_date=Sys.Date() - 1150
+input = read.csv('india_stock_list.csv', strip.white = TRUE, sep = ",", header = FALSE, stringsAsFactors=FALSE )
+
+#output file
+sink("india_stock_list_outfile.txt")
+for(i in 1:nrow(input))
+{
+  quote = input[i,1]
+  name = input[i,2]
+  tryCatch({
+    data=Quandl(quote, api_key="7BS7k1_dnKCP3h2FefxA", trim_start=start_date, type = "xts")
+    if (mean(tail(data,n=7)[,6]) > 50000){
+      if ( length(grep("CHRIS",quote))>0){
+        x=as.quantmod.OHLC(data[,1:4],col.names = c("Open", "High","Low", "Close"))
+        ic = ichimoku(xts(x[,1:4]),18,52,104)
+      } else{
+        ic = ichimoku(data[,1:4],18,52,104)
+      }
+    }
+      anyGood(ic,quote,name)},error = function(err) {
+      })
+}
+
+sink()
